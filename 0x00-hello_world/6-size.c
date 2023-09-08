082b5f4704e5f263670bff1e3e70b56734fbee28
evans-manyala <evans.manyala>
2023-09-07 18:07:36 -0700

    Adding C script that prints Data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-printf("The size of int is %d bytes\n", sizeof(int));
-printf("The size of char is %d bytes\n", sizeof(char));
-printf("The size of long int is %d bytes\n", sizeof(long int));
-printf("The size of float is %d bytes\n", sizeof(float));
-printf("The size of long is %d bytes\n", sizeof(long));
-return(0);
+	printf("Size of a char is %d byte(s)\n", sizeof(char));
+	printf("Size of an int is %d byte(s)\n", sizeof(int));
+	printf("Size of a long is %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int is  %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float is %d byte(s)\n", sizeof(float));
+	return (0);
 }
