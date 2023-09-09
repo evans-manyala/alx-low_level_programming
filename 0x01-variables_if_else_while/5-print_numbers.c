80dde0a564d4f1891d305ff6296a92761e70d7fc
evans-manyala <evans.manyala>
2023-09-08 18:56:54 -0700

    Addning new C scripts

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -  Starting block
+ *
+ * Return:  Success(0)
+ */
+int main(void)
+{
+int num  = 0;
+while num < 10;
+{
+printf("%d", num);
+num++;
+}
+printf("\n");
+return (0);
+}
