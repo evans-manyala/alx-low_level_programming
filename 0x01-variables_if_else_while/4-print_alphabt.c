16dfbafb1b79cf1c6dc89471eafe85c3fa3ae77e
evans-manyala <evans.manyala>
2023-09-08 18:32:34 -0700

    Addning new C scripts

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,17 +6,14 @@
  */
 int main(void)
 {
- char a2z;
-
- for (a2z = 'a'; a2z <= 'z'; a2z++) {
-  if (a2z == 'q' || a2z == 'e') {
-      continue;
-   }
-
-  putchar(a2z);
-  }
-
-  putchar('\n');
-
-  return 0;
+char a2z;
+for (a2z = 'a'; a2z <= 'z'; a2z++)
+{
+if (a2z == 'q' || a2z == 'e') {
+continue;
+}
+putchar(a2z);
+}
+putchar('\n');
+return 0;
 }
