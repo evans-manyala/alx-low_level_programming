5b67ee1f72453a1cdaca0f4a2efbbdc2e2676231
evans-manyala <evans.manyala>
2023-09-09 03:14:51 -0700

    Adding new C scripts

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -  Starting block
+ *
+ * Return:  Success(0)
+ */
+int main(void) {
+  int i, j;
+
+  for (i = 0; i < 100; i++) {
+    for (j = i + 1; j < 10; j++) {
+      putchar(i + '0');
+      putchar(j + '0');
+      if (i != 9 || j != 8) {
+        putchar(',');
+        putchar(' ');
+      }
+    }
+  }
+
+  return 0;
+}
