b7459c5b81825463db00dd727c6b67b33ec865bd
evans-manyala <evans.manyala>
2023-09-09 06:37:00 -0700

    Adding C scripts

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,25 +4,26 @@
  *
  * Return:  Success(0)
  */
-int main(void) 
-{
-int x, y, z;
-for (x = '0'; x <= '7'; x++)
-{
-for (y = x + 1; y <= '9'; y++)
-{
-z = y + 1;
-putchar(x);
-putchar(y);
-putchar(z);
-if (x == '7' && y == '8')
-{
-break;
+#include <stdio.h>
+
+int main(void) {
+  int x, y, z;
+
+  for (x = '0'; x <= '9'; x++) {
+    for (y = x + 1; y <= '9'; y++) {
+      z = y + 1;
+      if (x == '7' && y == '8') {
+        break;
+      }
+      putchar(x);
+      putchar(y);
+      putchar(z);
+      putchar(',');
+      putchar(' ');
+    }
+  }
+
+  putchar('\n');
+  return (0);
 }
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
 }
