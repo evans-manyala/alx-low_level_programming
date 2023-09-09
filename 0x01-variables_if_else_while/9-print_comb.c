4e11704cfddf1346568c8985336377bace148850
evans-manyala <evans.manyala>
2023-09-08 20:00:30 -0700

    Addingn script to princt numbers to base 16

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+/**
+ * main -  Starting block
+ *
+ * Return:  Success(0)
+ */
+int main(void)
+{
+for (num = '0'; num <= '9'; num++)
+{
+putchar(num);
+if (num == '9')
+{
+continue;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
