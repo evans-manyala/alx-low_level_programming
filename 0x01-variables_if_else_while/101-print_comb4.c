0794ac70bf30ae5b8701287c8d57ac2ac9e005a0
evans-manyala <evans.manyala>
2023-09-09 06:06:57 -0700

    Adding new C scripts

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main -  Starting block
+ *
+ * Return:  Success(0)
+ */
+int main(void)
+{
+        int i, j;
+
+        for (i = 0; i < 99 ; i++)
+        {
+                for (j = i + 1 ; j < 10 ; j++)
+                {
+                        putchar('0' + i);
+                        putchar('0' + j);
+                        if (i != 88 || j != 99)
+                        {
+                                putchar(',');
+                                putchar(' ');
+                        }
+
+                }
+}
+
+        putchar('\n');
+        return (0);
+}
