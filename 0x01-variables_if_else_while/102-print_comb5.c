f175a2255ae2737396778146a603ad79236e666a
evans-manyala <evans.manyala>
2023-09-09 07:24:49 -0700

    Adding C scripts

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Starting block
  * Return: Success (0)
@@ -8,27 +9,43 @@ int main(void)
 int a = 0, b = 0;
 
 while (a < 100)
+
 {
+b = 0;
 while (b < 100)
+
 {
-if (a < b
+
+if (a < b)
+
 {
+
 putchar((a / 10) + 48);
 putchar((a % 10) + 48);
 putchar(' ');
 putchar((b / 10) + 48);
 putchar((b % 10) + 48);
+
 if (a != 98 || b != 99)
+
 {
+
 putchar(',');
 putchar(' ');
+
 }
+
 }
+
 b++;
+
 }
+
 a++;
-b = 0;
-}
+
+}  
+
 putchar('\n');
 return (0);
+
 }
