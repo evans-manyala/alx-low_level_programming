e1f8f4d61750dad9285c5d018288d4d1dc341f05
evans-manyala <evans.manyala>
2023-09-12 16:57:53 -0700

    Adding C scripts with Functions and Nested Loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,16 +9,19 @@
  *
  * Return: 1 if 'c' is lower or upper case, 0 otherwise.
  */
+
 int _isalpha(int c)
 {
 	if (c >= 'a' && c <= 'Z')
 	{
 		return (1);
 	}
-	if (c >= 'A' || c <= 'Z')
+
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
+
 	else
 	{
 		return (0);
