de47577841a0546bf477f2f9460e57ecb1fd76c7
evans-manyala <evans.manyala>
2023-09-12 09:11:21 -0700

    Adding C scripts

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * Main - Program starting block
  * Description: This script prints _putchar followed by a new line
  * Return : 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	char var[8]="_putchar";
-	
+	char var[8] = "_putchar";
 	int x;
-	
-	for(x=0; x<8; x++)
+
+	for (x = 0; x < 8 ; x++)
 	{
 		_putchar(var[x]);
 	}
