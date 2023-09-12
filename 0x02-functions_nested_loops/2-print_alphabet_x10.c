8eb9e11ca131ffc4a54529a7cf94223b8eefaf4e
evans-manyala <evans.manyala>
2023-09-12 13:16:10 -0700

    Adding C scripts with Functions and Nested Loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints lowercase alphabets ten times.
+ * Description: This function prints the lowercase alphabet from 'a' to 'z' and
+ * then outputs a newline character.
+ * Return: On success (0), returns character written.
+ */
+void print_alphabet_x10(void)
+{
+	char var;
+	int num;
+
+	for (var = 'a'; var <= 'z' ; var++)
+	{
+		for (num = 1 ; num <= 10 ; num++)
+		{
+			_putchar(var);
+		}
+	}
+	_putchar('\n');
+}
