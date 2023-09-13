2b731b2a5cefc534341ad57d17bd3bee6ef0a38a
evans-manyala <evans.manyala>
2023-09-12 18:33:03 -0700

    Adding C Scripts with Functions and Nested Loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_last_digit - function that compute last digit of a number
+ *@n: number to be checked
+ *
+ * Return: last digit of a number
+ */
+
+int print_last_digit(int n)
+{
+	int rightmost_digit;
+
+	if (n < 0)
+	{
+		rightmost_digit = ((-1 * n) % 10);
+	}
+	else
+	{
+		rightmost_digit = (n % 10);
+		_putchar(48 + rightmost_digit);
+	}
+	return (rightmost_digit);
+}
