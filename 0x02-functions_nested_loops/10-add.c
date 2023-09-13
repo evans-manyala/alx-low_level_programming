9772c44701988dd885e7a9c32c0f269884cba6c1
evans-manyala <evans.manyala>
2023-09-13 07:12:34 -0700

    Adding C scripts with Functions and Nested Loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - This code adds two integers and returns the result.
+ * @num1: value to be added to num2.
+ * @num2: value to be added to num1.
+ * Return: sum of num1 and num2
+ *
+ *
+ */
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+	return (result);
+}
