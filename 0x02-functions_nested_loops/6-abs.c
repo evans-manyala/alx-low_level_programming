2f99c2690fe119452206f8f7c3b310327a9d7250
evans-manyala <evans.manyala>
2023-09-12 18:15:20 -0700

    Adding C scripts with Functions and Nested Loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - returns absolute of a number.
+ * @n: The number to get the <absolute value of.
+ * Return: The absolute of the number.
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
