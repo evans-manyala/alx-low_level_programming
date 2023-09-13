ee0965e034372b84460ccaa7b6229123d8f31764
evans-manyala <evans.manyala>
2023-09-13 16:12:00 -0700

    Adding script for Fibonacci sequence

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-int n = 50;
-unsigned long long fib[n];
+int n = 50, i;
+unsigned long fib[n];
 fib[0] = 1;
 fib[1] = 2;
 
-for (int i = 2; i < n; i++)
+for (i = 2; i < n; i++)
 {
 fib[i] = fib[i - 1] + fib[i - 2];
 }
-for (int i = 0; i < n - 1; i++)
+for (i = 0; i < n - 1; i++)
 {
 printf("%llu, ", fib[i]);
 }
