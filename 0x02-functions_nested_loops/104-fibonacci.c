d652a2345b130daccac8c049d9b59f2e51574f0d
evans-manyala <evans.manyala>
2023-09-13 16:38:01 -0700

    Adding script for printing frist 98 numbers in a Fibonacci sequnce

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints the first 98 fibonacci numbers.
+ * Return: Success (0).
+ *
+ */
+
+int main(void)
+{
+int f1 = 0, f2 = 1, n, i;
+
+printf("The first 98 Fibonacci numbers are: ");
+
+for (i = 0; i < 98; i++)
+{
+n = f1 + f2;
+printf("%d, ", n);
+f1 = f2;
+f2 = n;
+}
+printf("\n");
+return (0);
+}
