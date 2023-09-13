32facf194ef9949fcdee9653390803c63ffcef70
evans-manyala <evans.manyala>
2023-09-13 04:38:51 -0700

    Adding C scripts with Function and Nested Loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints every minute of the day
+ *
+ *
+ *
+ *
+ *
+ */
+void jack_bauer(void)
+{
+	int hh, mm;
+
+	for (hh = 0; hh < 24; hh++)
+	{
+		for (mm = 0; mm < 60; mm++)
+		{
+			_putchar((hh / 10) + '0');
+			_putchar((hh % 10) + '0');
+			_putchar(':');
+			_putchar((mm / 10) + '0');
+			_putchar((mm % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
