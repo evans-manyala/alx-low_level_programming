e6bc5dad26017ef30fc646d8e36a38449115b331
evans-manyala <evans.manyala>
2023-09-13 06:43:51 -0700

    Adding C Scripts with Functions and Nested Loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,46 +1,35 @@
 #include "main.h"
 /**
- * times_table - Prints the 9 times table starting with 0
- *
- *
- *
- *
- *
- *
- *
+ * times_table - prints the 9 times table, starting with 0
  */
-
 void times_table(void)
 {
-	int num = 0, num2, result;
+	int num, num2, result;
 
-	for (num = 0; num < 8; num++)
+	for (num = 0; num < 10; num++)
 	{
-		for (num2 = 0; num2 < 9; num2++)
+		for (num2 = 0; num2 < 10; num2++)
 		{
 			result = num * num2;
-
-			if (result < 10)
+			if (num2 == 0)
 			{
-				if (num2 > 0)
-				{
-					_putchar(' ');
-					_putchar(result + '0');
-					_putchar(' ');
-				}
-			else
-			{
-				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
+				_putchar(result + '0');
 			}
 
-			if (num2 < 9)
+			if (result < 10 && num2 != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
-			}
-			_putchar('\n');
+				_putchar(' ');
+				_putchar(result + '0');
+			} else if (result >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
 			}
 		}
+		_putchar('\n');
 	}
 }
