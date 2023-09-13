e02792fa59de68bcdf02a4111837696d32039c55
evans-manyala <evans.manyala>
2023-09-13 15:13:32 -0700

    Adding scrit 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - This script calculates sum of multiples of 3 and 5 below 1024
+ * Return: success (0)
+ */
+int main(void)
+{
+	int n = 0;
+	int sum = 0;
+
+	while (n < 1024)
+	{
+		if (n % 3 == 0 or n % 5 == 0)
+		{
+			sum = sum + n;
+		}
+	}
+
+	n++;
+
+	printf("Sum of multiple of 3 and 5 below 1024 is: ", sum);
+	return (0);
+}
+
+
