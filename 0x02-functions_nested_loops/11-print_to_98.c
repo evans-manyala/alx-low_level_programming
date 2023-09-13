0bb7914e2f575e336da7e2c753846dfad2c31fed
evans-manyala <evans.manyala>
2023-09-13 10:03:49 -0700

    Adding C Scripts with Functions and Nested Loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_to_98 - Prints all natural numbers from 1 to 98.
+ * @n: This represents all natural numbers.
+ *
+ *
+ *
+ */
+void print_to_98(int n)
+{
+	n = 0;
+
+	while (n <= 98)
+	{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+
+	}
+	n++;
+}
