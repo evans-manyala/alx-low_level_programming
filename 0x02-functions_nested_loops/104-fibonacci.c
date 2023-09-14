44f0d714d6882ed0e7499185fb4f9043e954ffff
evans-manyala <evans.manyala>
2023-09-13 18:16:32 -0700

    Adding script 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,20 +3,23 @@
  * main - prints the first 98 fibonacci numbers.
  * Return: Success (0).
  *
+ *
  */
-
 int main(void)
 {
-unsigned long int f1 = 0, f2 = 1, n;
+unsigned long int f1 = 1, f2 = 1, n;
 int i;
+printf("1, 1, ");
 
-for (i = 0; i < 97; i++)
+for (i = 2; i < 96; i++)
 {
 n = f1 + f2;
 printf("%lu, ", n);
 f1 = f2;
 f2 = n;
 }
-printf("%lu\n", f1 + f2);
+n = f1 + f2;
+printf("%lu, %lu\n", n, n + f2);
 return (0);
 }
+
