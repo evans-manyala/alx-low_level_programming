4fdb9bed7b7e62831e0841ec5f0db257cfd3b6cb
evans-manyala <evans.manyala>
2023-09-14 10:21:10 -0700

    Adding Function 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 and 9 followed by a new line
+ *
+ *
+ *
+ */
+void print_numbers(void)
+{
+	int num;
+
+	while (num <= 9)
+	{
+		_putchar(num + '0');
+		num++;
+	}
+	_putchar('\n');
+}
