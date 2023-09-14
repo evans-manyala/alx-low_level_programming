e6dddf360a95332db3904a4e905268a91457c7b7
evans-manyala <evans.manyala>
2023-09-14 15:24:48 -0700

    Adding Function 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,26 +8,15 @@ void print_diagonal(int n)
 {
 	int separator, length;
 
-	if (n > 0)
+	for (length = 0; length < n; length++)
 	{
-		for (length = 0; length < n; length++)
+		for (separator = 0; separator < length; separator++)
 		{
-			for (separator = 0; separator < length; separator++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-
-			length = n - 1;
-
-			if (length)
-			{
-				continue;
-			}
-
-			_putchar('\n');
+			_putchar(' ');
 		}
+		_putchar('\\');
+		if (length < (n - 1))
+			_putchar('\n');
 	}
-
 	_putchar('\n');
 }
