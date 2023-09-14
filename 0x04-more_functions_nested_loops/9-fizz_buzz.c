dcafde975eaea6d226c31ae1e2e1029660f53aca
evans-manyala <evans.manyala>
2023-09-14 16:23:40 -0700

    Adding Function 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Program prints numbers and 'Fizz' to represent
+ * multiples of 3 and 'FizzBuzz'for multiples of 5
+ * for numbers between 1 and 100.
+ * Return: Success (0).
+ */
+int main(void)
+{
+int x1;
+for (x1 = 1; x1 <= 100; x1++)
+{
+if (x1 % 3 == 0)
+{
+printf("Fizz");
+printf(" ");
+}
+else if (x1 % 5 == 0)
+{
+printf("FizBuzz");
+printf(" ");
+}
+else
+{
+printf("%d", +x1);
+printf(" ");
+}
+}
+return (0);
+}
