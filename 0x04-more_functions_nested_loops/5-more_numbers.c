06ac6c3af39ca004ebd1add5bfa28f46ae6c6874
evans-manyala <evans.manyala>
2023-09-14 12:37:33 -0700

    Adding Functions 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -18,8 +18,8 @@ void more_numbers(void)
 			if (num > 9)
 			{
 				_putchar((num2 / 10) + '0');
-				_putchar((num2 % 10) + '0');
 			}
+			_putchar((num2 % 10) + '0');
 		}
 		_putchar('\n');
 	}
