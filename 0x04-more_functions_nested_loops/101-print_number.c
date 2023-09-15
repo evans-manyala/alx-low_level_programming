33594e6bd90dd0fe9463a52803839dca7a8b8a6b
evans-manyala <evans.manyala>
2023-09-15 12:25:08 -0700

    Adding Function 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,45 +6,18 @@
  */
 void print_number(int n)
 {
-	int count, x, y, k;
+	unsigned int k = n;
 
-	x = 0, y = 1000000000;
-
-	if (n == 0)
-	{
-		_putchar('0');
-	}
-	else if (n > 0)
+	if (n < 0)
 	{
 		n *= -1;
-	}
-	else
-	{
+		k = n;
 		_putchar('-');
 	}
-	for (count = 0; count < 10; count++)
-	{
-		if (n / y == 0 && x == 0)
-		{
-			y /= 10;
-			continue;
-		}
-		else if (x == 0)
-		{
-			_putchar(-(n / y) + '0');
-			x += 1;
-		}
-		else
-		{
-			k = (-(n / y) % 10);
 
-			if (k < 0)
-			{
-				k *= -1;
-			}
+	k /= 10;
 
-			_putchar(k + '0');
-		}
-		y /= 10;
-	}
+	if (k != 0)
+		print_number(k);
+	_putchar((unsigned int) n % 10 + '0');
 }
