99bacf73bac1385ddea5da07294fa2c7dc1ef097
evans-manyala <evans.manyala>
2023-09-18 16:10:19 -0700

    Adding Function 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,35 +6,31 @@
  * Return: Intger value of the string or 0 if
  * no valid string is present.
  */
-
 int _atoi(char *s)
 {
-	int x1, z1 = 0;
-	unsigned int xy = 0;
-	int y1 = 1;
+	int yield = 1, rslt = 0, x1 = 0;
 
-	while (s[x1])
+	while (s[x1] == ' ')
 	{
-		if (s[x1] == 45)
-		{
-			y1 *= -1;
-		}
-
-		while (s[x1] >= 48 && s[x1] <= 57)
-		{
-			z1 = 1;
-			xy = (xy * 10) + (s[x1] - '0');
-			x1++;
-		}
-
-		if (z1 == 1)
-		{
-			break;
-		}
-
 		x1++;
 	}
-
-	xy *= y1;
-	return (xy);
+	if (s[x1] == '-')
+	{
+		yield -= 1;
+		x1++;
+	}
+	else if (s[x1] == '+')
+	{
+		x1++;
+	}
+	while (s[x1] >= '0' && s[x1] <= '9')
+	{
+		if (rslt > (INT_MAX - (s[x1] - '0')) / 10)
+		{
+			return ((yield == 1) ? INT_MAX : INT_MIN);
+		}
+		rslt = rslt * 10 + (s[x1] - '0');
+		x1++;
+		}
+	return (yield * rslt);
 }
