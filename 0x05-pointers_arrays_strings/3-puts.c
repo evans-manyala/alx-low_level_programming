cbe2a11dca78a36785e1e866c5c73d5255808750
evans-manyala <evans.manyala>
2023-09-18 05:49:53 -0700

    Adding script Function 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - function prints a string followed by a newline.
+ * @str: string value being returned followed by a new line.
+ * Return: void
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
