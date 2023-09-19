ef7566900d8825fc65a57b8c80087cf2542676c6
evans-manyala <evans.manyala>
2023-09-19 06:51:26 -0700

    Adding Function 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,35 +1,37 @@
 #include "main.h"
+#include <limits.h>
 /**
- * _atoi - Converts a string to an integer
- * @s: Pointer to the string to be converted
- * Return: The integer value of the string, or 0 if no valid integer is found
+ * _atoi - Converts a string to an integer.
+ * @s: Pointer to the string to be converteded.
+ * Return: Intger value of the string or 0 if
+ * no valid string is present.
  */
 int _atoi(char *s)
 {
-	int x1 = 1;
-	unsigned int sum = 0;
-	char flag = 0;
+	int yield = 1, rslt = 0, x1 = 0;
 
-	while (*s)
+	while (s[x1] == ' ')
 	{
-		if (*s == '-')
-		{
-			flag *= -1;
-		}
-		if (*s >= '0' && *s <= '9')
-		{
-			flag = 1;
-			sum = ((sum * 10) + *s) - '0';
-		}
-		else if (flag)
-		{
-			break;
-			s++;
-		}
-		if (x1 < 0)
-		{
-			sum = (-sum);
-		}
+		x1++;
 	}
-	return (sum);
+	if (s[x1] == '-')
+	{
+		yield -= 1;
+		x1++;
+	}
+	else if (s[x1] == '+')
+	{
+		x1++;
+	}
+	while (s[x1] >= '0' && s[x1] <= '9')
+	{
+		if (rslt > (INT_MAX - (s[x1] - '0')) / 10)
+		{
+			return ((yield == 1) ? INT_MAX : INT_MIN);
+		}
+		rslt = rslt * 10 + (s[x1] - '0');
+		x1++;
+		}
+	return (yield * rslt);
 }
+
