081f39c24280bfb5217c4f3d1d071fd301a8e28b
evans-manyala <evans.manyala>
2023-09-19 11:31:11 -0700

    Adding Function 100-ato1.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,31 +1,36 @@
 #include "main.h"
+#include <limits.h>
 /**
  * _atoi - convert a string to an integer.
  * @s: Point variable to be parsed.
  * Return: results of the conversion.
  */
-
 int _atoi(char *s)
 {
-	int rslt, x1 = 0;
+	int rslt = 0;
+	int x1 = 0;
 	int sgn = 1;
-
-	while (s[x1] == ' ' || s[x1] == '-' || s[x1] == '+')
+	
+	while (s[x1] == ' ')
 	{
-		if (s[x1] == '-')
-		{
-			sgn = -1;
-		}
+		x1++;
+	}
+	if (s[x1] == '-' || s[x1] == '+')
+	{
+		sgn = (s[x1] == '-') ? -1 : 1;
 		x1++;
 	}
 
-	if (s[x1] >= '0' && s[x1] <= '9')
+	while (s[x1] >= '0' && s[x1] <= '9')
 	{
-		while (s[x1] >= '0' && s[x1] <= '9')
+		if (rslt > (INT_MAX - (s[x1] - '0')) / 10)
 		{
-			rslt = rslt * 10 + (s[x1] - '0');
-			x1++;
+			return ((sgn == 1) ? INT_MAX : INT_MIN);
 		}
+
+		rslt = rslt * 10 + (s[x1] - '0');
+		x1++;
 	}
 	return (rslt * sgn);
 }
+
