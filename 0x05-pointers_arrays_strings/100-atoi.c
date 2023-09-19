64beb935ba703fb085a4f8176b2fa33cb21252d2
evans-manyala <evans.manyala>
2023-09-19 11:17:37 -0700

    Adding Function 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,34 +7,25 @@
 
 int _atoi(char *s)
 {
-	int rslt = 0, x1 = 0;
+	int rslt, x1 = 0;
 	int sgn = 1;
 
-	while (s[x1] == ' ')
+	while (s[x1] == ' ' || s[x1] == '-' || s[x1] == '+')
 	{
-		x1++;
-	}
-
-	if (s[x1] == '-' || s[x1] == '+')
-	{
-		sgn = -1;
-
 		if (s[x1] == '-')
 		{
-			x1++;
+			sgn = -1;
 		}
-
-		else
-		{
-			sgn = 1;
-		}
-	}
-
-	while (s[x1] >= '0' && s[x1] <= '9')
-	{
-		rslt = rslt * 10 + (s[x1] - '0');
 		x1++;
 	}
 
+	if (s[x1] >= '0' && s[x1] <= '9')
+	{
+		while (s[x1] >= '0' && s[x1] <= '9')
+		{
+			rslt = rslt * 10 + (s[x1] - '0');
+			x1++;
+		}
+	}
 	return (rslt * sgn);
 }
