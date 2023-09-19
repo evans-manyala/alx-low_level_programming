745cacd224c29c31625363899081dbef9d0dd879
evans-manyala <evans.manyala>
2023-09-19 13:30:51 -0700

    Adding Function 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,28 +1,32 @@
 #include "main.h"
 #include <limits.h>
 /**
- * _atoi - convert a string to an integer.
- * @s: Point variable to be parsed.
- * Return: results of the conversion.
+ * _atoi - Converts a string to an integer
+ * @s: Pointer to the string to be converted
+ * Return: The integer value of the string, or 0 if no valid integer is found
  */
 int _atoi(char *s)
 {
 	int rslt = 0;
 	int x1 = 0;
 	int sgn = 1;
+	int final_digit = 0;
 
 	while (s[x1] == ' ')
 	{
 		x1++;
 	}
-	if (s[x1] == '-' || s[x1] == '+')
+
+	while (s[x1] == '-' || s[x1] == '+')
 	{
-		sgn = (s[x1] == '-') ? -1 : 1;
+		sgn *= (s[x1] == '-') ? -1 : 1;
 		x1++;
 	}
 
 	while (s[x1] >= '0' && s[x1] <= '9')
 	{
+		final_digit = 1;
+
 		if (rslt > (INT_MAX - (s[x1] - '0')) / 10)
 		{
 			return ((sgn == 1) ? INT_MAX : INT_MIN);
@@ -31,6 +35,11 @@ int _atoi(char *s)
 		rslt = rslt * 10 + (s[x1] - '0');
 		x1++;
 	}
+
+	if (!final_digit)
+	{
+		return (0);
+	}
+
 	return (rslt * sgn);
 }
-
