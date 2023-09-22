dad55c3bc9988b3c7b2354af1e62a000c0c68e4d
evans-manyala <evans.manyala>
2023-09-22 05:28:11 -0700

    Fixing error with script 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,22 +6,18 @@
  */
 void print_number(int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
+	int x = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		x = -n;
 	}
 
 	if (n / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(x / 10);
 	}
 
-	_putchar('0' + (n % 10));
+	_putchar('0' + (x % 10));
 }
