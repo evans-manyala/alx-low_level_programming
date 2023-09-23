5f1d4a7240d2debe832551342bcec9c6cbc37efb
evans-manyala <evans.manyala>
2023-09-22 20:00:44 -0700

    Fixing error with script 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,46 +1,5 @@
+#include "main.h"
 #include <stdio.h>
-#include <ctype.h>
-/**
- * print_hex_line - Helper function to print a single line of hexadecimal.
- * @b: The buffer to print.
- * @start: The starting index for the line.
- * @end: The ending index for the line.
- */
-void print_hex_line(char *b, int start, int end)
-{
-	int x;
-
-	for (x = start; x < end; x++)
-	{
-		printf("%02x", (unsigned char)b[x]);
-		if (x % 2 == 1)
-		{
-			printf(" ");
-		}
-	}
-}
-/**
- * print_char_line - Helper function to print a single line of characters.
- * @b: The buffer to print.
- * @start: The starting index for the line.
- * @end: The ending index for the line.
- */
-void print_char_line(char *b, int start, int end)
-{
-	int x;
-
-	for (x = start; x < end; x++)
-	{
-		if (isprint(b[x]))
-		{
-			printf("%c", b[x]);
-		}
-		else
-		{
-			printf(".");
-		}
-	}
-}
 
 /**
  * print_buffer - Prints the content of a buffer in a specified format.
@@ -49,20 +8,39 @@ void print_char_line(char *b, int start, int end)
  */
 void print_buffer(char *b, int size)
 {
-	int x;
+	int offset = 0, x, y, z;
 
 	if (size <= 0)
 	{
 		printf("\n");
 		return;
 	}
-
-	for (x = 0; x < size; x += 10)
+	while (offset < size)
 	{
-		printf("%08x: ", x);
-		print_hex_line(b, x, x + 10);
-		printf(" ");
-		print_char_line(b, x, x + 10);
-		printf("\n");
+		y = (size - offset < 10) ? (size - offset) : 10;
+		printf("%08x: ", offset);
+		for (x = 0; x < 10; x++)
+		{
+			if (x < y)
+				printf("%02x", *(b + offset + x));
+			else
+				printf("  ");
+			if (x % 2 == 1)
+			{
+				printf(" ");
+			}
+		}
+
+			for (x = 0; x < y; x++)
+			{
+				z = *(b + offset + x);
+				if (z < 32 || z > 132)
+				{
+					z = '.';
+				}
+				printf("%c", z);
+			}
+			printf("\n");
+			offset += 10;
 	}
 }
