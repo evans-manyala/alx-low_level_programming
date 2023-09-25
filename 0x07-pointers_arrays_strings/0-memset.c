5f44b7b106049bf28687e4370db28b6c22819633
evans-manyala <evans.manyala>
2023-09-25 06:01:52 -0700

    Adding script 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _memset - Function fills memory with constant byte.
+ * @s: A pointer to the memory area to be filled.
+ * @b: The constanst bute to fill the memory area.
+ * @n: The number of bytes to fill.
+ * Return: value to return.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x;
+	
+	if (s == NULL || n == 0)
+	{
+		return (s);
+	}
+
+	for (x = 0; x < n; x++)
+	{
+		s[x] = b;
+	}
+
+	return (s);
+}
