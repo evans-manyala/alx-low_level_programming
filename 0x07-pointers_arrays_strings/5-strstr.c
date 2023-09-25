53f0589651e51f1983cdca3638c330d1de487388
evans-manyala <evans.manyala>
2023-09-25 13:20:41 -0700

    Adding 5-strstr.c script file

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strstr - Function locates a substring.
+ * @haystack: Pointer to a string to searched.
+ * @needle: Pointer to the substring being to be searched for.
+ * Return: valueif located in the haystack; otherwise NULL.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+		{
+			while (*needle != '\0' && *haystack == *needle)
+			{
+				haystack++;
+				needle++;
+			}
+
+			if (*needle == '\0')
+			{
+				return (haystack);
+			}
+		}
+
+		haystack++;
+	}
+
+	return (NULL);
+}
