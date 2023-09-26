2032772305d2672b76d55f6524bba8f31f6655ed
evans-manyala <evans.manyala>
2023-09-25 17:21:00 -0700

    Adding 7-print_chessboard.c script file

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_chessboard - Prints the chessboard.
+ * @a: characters representing the chessboard.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x, y = 0;
+
+	while (x < 8)
+	{
+		while (y < 8)
+		{
+			_putchar(a[x][y]);
+			y++;
+		}
+
+		_putchar('\n');
+		x++;
+	}
+}
