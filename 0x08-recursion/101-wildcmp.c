85e3fb9f2271e8aa923ccfd31ccc0eb1eb6be18a
evans-manyala <evans.manyala>
2023-09-27 15:45:49 -0700

    Ficing errors in 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,7 +6,7 @@
  * Return: 1 if the strings can be considered identical, otherwise 0.
  *
  */
-int wildcmp(char *s1, char *s2)
+int wildcmp(char *s1, char *s2) 
 {
 	if (*s1 == '\0' && *s2 == '\0')
 	{
