469ab5c1987a6df9281be1e4d6d24ce259bebd7c
evans-manyala <evans.manyala>
2023-09-27 06:47:14 -0700

    Adding 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion(char - Function prints length of a string.
+ * @s: String length to be determined.
+ * Return: String length after check.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
