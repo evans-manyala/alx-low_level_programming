4f681c93ce65ef363e7a65bd6584660d32569abb
evans-manyala <evans.manyala>
2023-09-27 12:34:35 -0700

    Adding 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * _sqrt_recursion - Prints the square root of a number.
- * @n: base number.
+ * @n: base number to calculate squareroot of.
+ * @num1:  Current square root guess pattern.
  * Return: Square root of the number as long as its not equal to 0.
  */
 int _sqrt_helper_function(int n, int num1);
@@ -25,7 +26,6 @@ int _sqrt_recursion(int n)
 /**
  * _sqrt_helper_function - Function calculates the square root recusively.
  * @n: base number to calculate square root of.
- * @num1:  Current square root guess pattern.
  * Return: Square root of 'n'.
  */
 
