bd77cff4bc656542f6436fd1fc8db08ce479216d
evans-manyala <evans.manyala>
2023-09-28 01:32:24 -0700

    Fixing logical errors in 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,19 +3,35 @@
 /**
  * is_palindrome - Function tests if a string is a palindrome.
  * @s: string being tested.
+ * @finish: The last index.
+ * @start: The start index.
  * Return: 1 if string 's' is a palindrome, otherwise 0
  */
-
+int is_palindrome_helper_function(char *s, int start, int finish);
 int is_palindrome(char *s)
 {
-	if (*s == '\0')
-	{
-		return (1);
-	}
-	if (strlen(s) <= 1)
-	{
-		return (1);
-	}
+	int l = strlen(s);
 
-	return (s[0] == s[strlen(s) - 1] && is_palindrome(s + 1));
+	return (is_palindrome_helper_function(s, 0, l - 1));
+}
+/**
+ * is_palindrome_helper_function - Recursive helper function to check
+ * if a string is a palindrome.
+ * @s: string to be tested.
+ * @start: The start index.
+ * @finish: The last index.
+ * Return: 1 if string is palindrome, otherwise 0.
+ */
+
+int is_palindrome_helper_function(char *s, int start, int finish)
+{
+	if (start >= finish)
+	{
+		return (1);
+	}
+	if (s[start] == s[finish])
+	{
+		return (is_palindrome_helper_function(s, start + 1, finish - 1));
+	}
+	return (0);
 }
