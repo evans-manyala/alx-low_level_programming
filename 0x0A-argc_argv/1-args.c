c2e10ec3a38dee9483d997db8dda3d487110dd8b
evans-manyala <evans.manyala>
2023-09-30 12:03:29 -0700

    Fixing logical errors in script

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,11 +10,8 @@
 
 int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
+	(void) argv;
 
-	if (argc >= 0)
-	{
-		printf("Name: %s\n", argv[0]);
-	}
+	printf("%d\n", argc - 1);
 	return (0);
 }
