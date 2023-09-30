0d25244664bc972eeff6d71f481091eb4423b1df
evans-manyala <evans.manyala>
2023-09-30 09:33:08 -0700

    Adding 1-args.c script

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - C program to print the number of arguments passed to it.
+ * @argc: integer variable that stores the number of command-line
+ * arguments passed by the user including the name of the program.
+ * @argv: An array null-terminated strings representing command-line arguments
+ * entered by the user of the program.
+ * Return: 0 if successful.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	if (argc >= 0)
+	{
+		printf("Name: %s\n", argv[0]);
+	}
+	return (0);
+}
