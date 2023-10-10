b973640e4baea5094483e64c288e2cd34f49a437
evans-manyala <evans.manyala>
2023-10-10 10:57:54 -0700

    Adding Error fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,31 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * new_dog
-*/
+ * new_dog - Creates a new dog with the given attributes.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog.
+ * Return: A pointer to the newly created dog,
+ * or NULL if memory allocation fails.
+ */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-        dog_t *dog_ptr = malloc(sizeof(dog_t));
+	dog_t *dog_ptr = malloc(sizeof(dog_t));
 
-    if (dog_ptr == NULL)
-    {
-        return NULL;
-    }
+	if (dog_ptr == NULL)
+	{
+		return (NULL);
+	}
 
-    dog_ptr->name = strdup(name);
-    dog_ptr->owner = strdup(owner);
+	dog_ptr->name = strdup(name);
+	dog_ptr->owner = strdup(owner);
 
-    if (dog_ptr->name == NULL || dog_ptr->owner == NULL)
-    {
-        free(dog_ptr->name); 
-        free(dog_ptr->owner);
-        free(dog_ptr);
-        
-        return NULL;
-    }
+	if (dog_ptr->name == NULL || dog_ptr->owner == NULL)
+	{
+		free(dog_ptr->name);
+		free(dog_ptr->owner);
+		free(dog_ptr);
 
-    dog_ptr->age = age;
+		return (NULL);
+	}
 
-    return (dog_ptr);
+	dog_ptr->age = age;
+	return (dog_ptr);
 }
