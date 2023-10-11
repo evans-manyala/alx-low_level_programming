a0dd31d5294c155fd43143bf5d388e1ae1120803
evans-manyala <evans.manyala>
2023-10-11 12:41:21 -0700

    Fixing logical error in code

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,16 +13,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int x;
 
-	if (size <= 0)
+	if (array && cmp && size > 0)
 	{
-		return (-1);
-	}
-
-	for (x = 0; x < size; x++)
-	{
-		if (cmp(array[x]) != 0)
+		for (x = 0; x < size; x++)
 		{
-			return (x);
+			if (cmp(array[x]))
+			{
+				return (x);
+			}
 		}
 	}
 
