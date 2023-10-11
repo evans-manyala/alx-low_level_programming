613f14e997d8c7de09baf50ba7e8fc99b6b1d84b
evans-manyala <evans.manyala>
2023-10-11 06:50:34 -0700

    Adding functions_pointers.h and 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name.
+ * @name: pointer to a character array containing the name to be printed.
+ * @f: pointer that takes a character pointer as an argument
+ * for printing the name.
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
