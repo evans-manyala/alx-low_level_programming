2f404fd132ea7311469cd77c40a58897b57e812f
evans-manyala <evans.manyala>
2023-10-19 07:57:44 -0700

    Adding header files and function

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: Pointer to a pointer to the head of the linked list.
+ * @str: String to be duplicated and added to the new node.
+ * Return: The address of the new element, or NULL if it failed.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	if (str == NULL)
+		return (NULL);
+
+	list_t *new_n = malloc(sizeof(list_t));
+
+	if (new_n == NULL)
+		return (NULL);
+
+	new_n->str = strdup(str);
+	
+	if (new_n->str == NULL)
+	{
+		free(new_n);
+		return (NULL);
+	}
+	new_n->len = strlen(str);
+	new_n->next = *head;
+	*head = new_n;
+	
+	return (new_n);
+}
