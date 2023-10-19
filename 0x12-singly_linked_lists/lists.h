967f85cdfc3890527b00c717a5663ef4ce905301
evans-manyala <evans.manyala>
2023-10-19 06:26:41 -0700

    Adding header files

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,7 +2,30 @@
 #define LISTS_H
 
 #include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
 
+/**
+ * struct node - Defines the node
+ * @nxt: defines the next item
+ * @d: data value.
+ */
+
+typedef struct node
+{
+	int d;
+	struct node *nxt;
+} node_t;
+
+/**
+ * struct list - Defines the list
+ * @hud: defines the head of the list.
+ */
+
+typedef struct list
+{
+	node_t *hud;
+} list_t;
 
 size_t print_list(const list_t *h);
 
