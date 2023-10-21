599f67ee03d409aa1952dff4dd207d77fde11f74
evans-manyala <evans.manyala>
2023-10-20 19:56:54 -0700

    Final logical error fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,6 +10,11 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_n;
 
+	if (head == NULL)
+		return (NULL);
+	if (str == NULL)
+		return (NULL);
+
 	new_n = malloc(sizeof(list_t));
 
 	if (new_n == NULL)
