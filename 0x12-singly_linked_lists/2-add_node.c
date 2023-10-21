5bb8e7ffab6fb37c3ae1ee9f1931d469356fe72a
evans-manyala <evans.manyala>
2023-10-20 19:51:42 -0700

    Final logical error fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,29 +6,26 @@
  * @str: String to be duplicated and added to the new node.
  * Return: The address of the new element, or NULL if it failed.
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_n;
 
-	if (str == NULL)
-		return (NULL);
-	if (strdup(str) == NULL)
-		return (NULL);
-
 	new_n = malloc(sizeof(list_t));
-	
+
 	if (new_n == NULL)
+	{
 		return (NULL);
+	}
 
 	new_n->str = strdup(str);
-	new_n->len = _strlen(str);
 
-	if (head == NULL)
-		new_n->next = NULL;
-	else
-		new_n->next = *head;
+	if (new_n->str == NULL)
+	{
+		free(new_n);
+		return (NULL);
+	}
 
+	new_n->next = *head;
 	*head = new_n;
 
 	return (new_n);
