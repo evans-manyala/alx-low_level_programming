4ce995a9edc6e10e063b335c7789463056cf50e5
evans-manyala <evans.manyala@gmail.com>
2023-10-25 14:15:44 +0300

    Adding file

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_listint_safe - Function frees a linked list.
+ * @h: Pointer to a pointer for the head node.
+ * Return: Freed list.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t x = 0;
+	listint_t *t;
+
+	while (*h != NULL)
+	{
+		t = *h;
+		*h = (*h)->next;
+
+		free(t);
+		x++;
+	}
+
+	*h = NULL;
+	return (x);
+}
