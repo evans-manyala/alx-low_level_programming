5f66255f670b561a29660f20d8bb641152596c4d
evans-manyala <evans.manyala@gmail.com>
2023-10-25 14:07:53 +0300

    Adding logical error fix and helper function

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,29 +1,67 @@
 #include "lists.h"
 
 /**
+ * free_listhelper - Helper function to free a linked list
+ * @head: start node so of a list.
+ */
+
+void free_listhelper(listh_s **head)
+{
+	listh_s *t, *pos;
+
+	if (head != NULL)
+	{
+		pos = *head;
+
+		while ((t = pos) != NULL)
+		{
+			pos = pos->next;
+			free(t);
+		}
+		*head = NULL;
+	}
+}
+
+/**
  * print_listint_safe - Function prints the number of nodes in a linked
  * list using a loop and handles infinite loops.
  * @head: Pointer to the head node in the linked list.
  * Return: Number of nodes in the list.
  */
+
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t x = 0;
-	const listint_t *pos = head;
-	const listint_t *nxtNode;
+	listh_s *headptr, *nw, *s;
 
-	while (pos != NULL)
+	headptr = NULL;
+	while (head != NULL)
 	{
-		if (pos->next == pos)
-		{
-			printf("** Error: Infinite loop detected in linked list **\n");
-			exit(98);
-		}
+		nw = malloc(sizeof(listh_s));
 
-		printf("[%p] %d\n", (void *)pos, pos->n);
-		nxtNode = pos->next;
-		pos = nxtNode;
+		if (nw == NULL)
+			exit(98);
+		nw->ptr = (void *)head;
+		nw->next = headptr;
+		headptr = nw;
+
+		s = headptr;
+
+		while (s->next != NULL)
+		{
+			s = s->next;
+
+			if (head == s->ptr)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listhelper(&headptr);
+				return (x);
+			}
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 		x++;
 	}
+	free_listhelper(&headptr);
 	return (x);
 }
