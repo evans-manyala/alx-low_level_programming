454609296bd00416984ef7dca66027540bb0fc6b
evans-manyala <evans.manyala@gmail.com>
2023-10-31 17:19:21 +0300

    Error fix application

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,33 +19,37 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fp_from = open(argv[1], O_RDONLY);
 	if (fp_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[1]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-
-	fp_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0666);
+	fp_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (fp_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 		exit(99);
 	}
-
-	bytes_read = read(fp_from, buffer, BUFFER_SIZE);
-	while (bytes_read > 0)
+	while ((bytes_read = read(fp_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_write = write(fp_to, buffer, bytes_read);
-		if (bytes_write != bytes_read)
+		if (bytes_written != bytes_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 			exit(99);
 		}
 	}
-	close(fp_from);
-	close(fp_to);
-
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (close(fp_from) == -1 || close(fp_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
+				(close(fp_from) == -1) ? fp_from : fp_to);
+		exit(100);
+	}
 	return (0);
 }
