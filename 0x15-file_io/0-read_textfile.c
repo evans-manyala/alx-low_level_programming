eacc2daf780781333d72a1dd4013d85edb46e085
evans-manyala <evans.manyala@gmail.com>
2023-10-31 06:22:47 +0300

    Fixing compile time and syntax errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,25 +9,39 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char c[letters];
+	char *buffer = malloc(letters * sizeof(char));
+	char *c = malloc(letters * sizeof(char));
 	ssize_t bytes_read;
 	FILE *fp;
 
 	if (filename == NULL)
+	{
+		free(c);
+		free(buffer);
 		return (0);
+	}
 
 	fp = fopen(filename, "r");
 
 	if (fp == NULL)
+	{
+		free(c);
+		free(buffer);
 		return (0);
+	}
 
 	bytes_read = fread(buffer, 1, letters, fp);
 	fclose(fp);
 
 	if (bytes_read < 0)
+	{
+		free(c);
+		free(buffer);
 		return (0);
+	}
 
 	fwrite(buffer, 1, bytes_read, stdout);
-
+	free(c);
+	free(buffer);
 	return (bytes_read);
 }
