7762e73f28a8e1ea1ffa19041eb1e54ed6c6344a
evans-manyala <evans.manyala@gmail.com>
2023-10-31 11:17:32 +0300

    Reverting to previous codee implementation

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,32 +6,37 @@
  * Return: Amount of data read and printed to stdout, otherwise 0
  * if the file is empty.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fp;
-	ssize_t bytes_read, bytes_write;
-	char *buffer;
+	char *buffer = malloc(letters * sizeof(char));
+	ssize_t bytes_read;
+	FILE *fp;
 
-	if (!filename)
+	if (filename == NULL)
+	{
+		free(buffer);
 		return (0);
+	}
 
-	fp = open(filename, O_RDONLY);
+	fp = fopen(filename, "r");
 
-	if (fp == -1)
+	if (fp == NULL)
+	{
+		free(buffer);
 		return (0);
+	}
 
-	buffer = malloc(sizeof(char) * (letters));
-	if (!buffer)
+	bytes_read = fread(buffer, 1, letters, fp);
+	fclose(fp);
+
+	if (bytes_read < 0)
+	{
+		free(buffer);
 		return (0);
+	}
 
-	bytes_read = read(fp, buffer, letters);
-	bytes_write = write(STDOUT_FILENO, buffer, bytes_read);
-
-	close(fp);
-
+	fwrite(buffer, 1, bytes_read, stdout);
 	free(buffer);
-
-	printf("Bytes written: %lu\n", bytes_write);
-
 	return (bytes_read);
 }
