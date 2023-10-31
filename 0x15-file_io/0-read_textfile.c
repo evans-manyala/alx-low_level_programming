26eb6f83fe3e86d12cd550f29ea83d8481288564
evans-manyala <evans.manyala@gmail.com>
2023-10-31 08:26:07 +0300

    Adding logical erro fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * read_textfile - Function reads a files and prints it to stdout
  * @filename: Pointer to the file to be read.
@@ -10,13 +11,11 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer = malloc(letters * sizeof(char));
-	char *c = malloc(letters * sizeof(char));
 	ssize_t bytes_read;
 	FILE *fp;
 
 	if (filename == NULL)
 	{
-		free(c);
 		free(buffer);
 		return (0);
 	}
@@ -25,7 +24,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (fp == NULL)
 	{
-		free(c);
 		free(buffer);
 		return (0);
 	}
@@ -35,13 +33,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (bytes_read < 0)
 	{
-		free(c);
 		free(buffer);
 		return (0);
 	}
 
 	fwrite(buffer, 1, bytes_read, stdout);
-	free(c);
 	free(buffer);
 	return (bytes_read);
 }
