3b18a54d5a1bd948218857421148f61a2fb691ee
evans-manyala <evans.manyala@gmail.com>
2023-10-31 06:00:07 +0300

    Adding file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * read_textfile - Function reads a files and prints it to stdout
+ * @filename: Pointer to the file to be read.
+ * @letters: Characters holdup information
+ * Return: Amount of data read and printed to stdout, otherwise 0
+ * if the file is empty.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char c[letters];
+	ssize_t bytes_read;
+	FILE *fp;
+
+	if (filename == NULL)
+		return (0);
+
+	fp = fopen(filename, "r");
+
+	if (fp == NULL)
+		return (0);
+
+	bytes_read = fread(buffer, 1, letters, fp);
+	fclose(fp);
+
+	if (bytes_read < 0)
+		return (0);
+
+	fwrite(buffer, 1, bytes_read, stdout);
+
+	return (bytes_read);
+}
