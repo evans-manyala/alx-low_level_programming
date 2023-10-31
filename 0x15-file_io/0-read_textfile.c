d67d8d1b04b62fd5bc7c1e97aa8eb13e50186ead
evans-manyala <evans.manyala@gmail.com>
2023-10-31 11:05:03 +0300

    Adding logical error fix for checker 9

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * read_textfile - Function reads a files and prints it to stdout
  * @filename: Pointer to the file to be read.
@@ -7,37 +6,32 @@
  * Return: Amount of data read and printed to stdout, otherwise 0
  * if the file is empty.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer = malloc(letters * sizeof(char));
-	ssize_t bytes_read;
-	FILE *fp;
+	int fp;
+	ssize_t bytes_read, bytes_write;
+	char *buffer;
 
-	if (filename == NULL)
-	{
-		free(buffer);
+	if (!filename)
 		return (0);
-	}
 
-	fp = fopen(filename, "r");
+	fp = open(filename, O_RDONLY);
 
-	if (fp == NULL)
-	{
-		free(buffer);
+	if (fp == -1)
 		return (0);
-	}
 
-	bytes_read = fread(buffer, 1, letters, fp);
-	fclose(fp);
-
-	if (bytes_read < 0)
-	{
-		free(buffer);
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
 		return (0);
-	}
 
-	fwrite(buffer, 1, bytes_read, stdout);
+	bytes_read = read(fp, buffer, letters);
+	bytes_write = write(STDOUT_FILENO, buffer, bytes_read);
+
+	close(fp);
+
 	free(buffer);
+
+	printf("Bytes written: %lu\n", bytes_write);
+
 	return (bytes_read);
 }
