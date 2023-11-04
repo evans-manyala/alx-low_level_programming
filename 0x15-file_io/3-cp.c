665e3aac966cfdf441b0e255b14eb3f303b19240
evans-manyala <evans.manyala@gmail.com>
2023-11-04 05:59:59 +0300

    Reversing changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,81 +1,52 @@
 #include "main.h"
 
 /**
- * error_handler - Function handles errors within the program.
- * @message: Error message to be printed.
- * @exit_code: Program exit code when an error occurs.
-*/
-
-void error_handler(const char *message, int exit_code)
-{
-	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(exit_code);
-}
-
-/**
- * file_copy - Function copies file
- * @src_filename: Source text file to be copied.
- * @dest_filename: Destination text file copied to.
- *
- * Return: 0 is success.
-*/
-
-int file_copy(const char *src_filename, const char *dest_filename)
-{
-	int src_fp, dest_fp;
-	ssize_t bytes_read, bytes_write;
-	char buffer[BUFFER_SIZE];
-
-	src_fp = open(src_filename, O_RDONLY);
-	if (src_fp == -1)
-	{
-		error_handler("Error: Can't read from source file", 98);
-	}
-
-	dest_fp = open(dest_filename, O_WRONLY | O_TRUNC | O_CREAT, 0664);
-
-	if (dest_fp == -1)
-	{
-		error_handler("Error: Can't write to destination file", 99);
-	}
-
-	while ((bytes_read = read(src_fp, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_write = write(dest_fp, buffer, bytes_read);
-		if (bytes_write != bytes_read)
-		{
-			error_handler("Error: Can't write to destination file", 99);
-		}
-	}
-
-	if (bytes_read == -1)
-	{
-		error_handler("Error: Can't read from source file", 98);
-	}
-
-	if (close(src_fp) == -1 || close(dest_fp) == -1)
-	{
-		error_handler("Error: Can't close a file descriptor", 100);
-	}
-
-	return (0);
-}
-
-/**
  * main - Function writes to a file
  * @argv: Array pointer of characters.
  * @argc: No. of arguments being passed.
- * * Return: 0 is success.
+ * Return: 0 is success.
+ *
  */
 
 int main(int argc, char *argv[])
 {
+	int fp_to, fp_from;
+	ssize_t bytes_read, bytes_write;
+	char buffer[BUFFER_SIZE];
+
 	if (argc != 3)
 	{
-		error_handler("Usage: cp file_from file_to", 97);
+		fprintf(stderr, "Usage: cp file_from file_to\n");
+		return (1);
 	}
 
-	file_copy(argv[1], argv[2]);
+	fp_from = open(argv[1], O_RDONLY);
 
+	if (fp_from == -1)
+	{
+		perror("open");
+
+		return (1);
+	}
+
+	fp_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (fp_to == -1)
+	{
+		perror("open");
+		return (1);
+	}
+
+	while ((bytes_read = read(fp_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_write = write(fp_to, buffer, bytes_read);
+		if (bytes_write != bytes_read)
+		{
+			perror("write");
+			return (1);
+		}
+	}
+
+	close(fp_from);
+	close(fp_to);
 	return (0);
-}}
+}
