615413f6bbe10ffddcd2a6eeaa664b7a158f4e9d
evans-manyala <evans.manyala@gmail.com>
2023-11-08 12:38:18 +0300

    Adding header files

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,6 +11,10 @@
 #include <elf.h>
 
 #define BUFFER_SIZE 1024
+#define EI_NIDENT 16
+
+typedef unsigned char byte;
+
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
