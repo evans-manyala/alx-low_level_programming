cbf942c373b24bea3999cbbfe03b7e605bdd3181
Evans Manyala <evans.manyala@gmail.com>
2023-12-08 01:49:01 +0300

    Fixing header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,8 +1,10 @@
-#ifdef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 
 #include <string.h>
+
 #include <stdlib.h>
+
 #include <stdio.h>
 
 /**
@@ -16,15 +18,17 @@
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+        int n;
+        struct dlistint_s *prev;
+        struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
-dlistint_t *add_dnodeint(dlistint_t **head, const int n);
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint(dlistint_t **head,
+                         const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head,
+                             const int n);
 void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
