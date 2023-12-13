0ebd56b526b4405161e0c84450619d90239b2ea7
Evans Manyala <evans.manyala@gmail.com>
2023-12-13 14:50:04 +0300

    Adding strncpy() function

--- 0x18-dynamic_libraries/2-strncpy.c
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -1,32 +1,20 @@
 #include "main.h"
 /**
- * _strspn - Function finds the first occurrence
- * of a character in string1 that is not contained
- * in the set of characters that is specified by string2
- * @s: Pointer to the character being checked.
- * @accept: Pointer to the Character being after being found.
- * Return: Length of string string 1.
+ * _strncpy - Function copies string from source
+ * to destination.
+ * @src: Pointer to the source string to be copied.
+ * @dest: Pointer to the new destination string address.
+ * @n: String length
+ * Return: Pointer of the new address.
  */
-unsigned int _strspn(char *s, char *accept)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int count = 0, found = 0;
-
-	while (*s)
+	char *ptr = dest;
+	while (n-- > 0 && (*dest++ = *src++) != '\0')
+		;
+	while (n-- > 0)
 	{
-		for (char *p = accept; *p; p++)
-		{
-			if (*s == *p)
-			{
-				found = 1;
-				break;
-			}
-		}
-		if (!found)
-		{
-			break;
-		}
-		count++;
-		s++;
+		*dest++ = '\0';
 	}
-	return (count);
+	return (ptr);
 }
