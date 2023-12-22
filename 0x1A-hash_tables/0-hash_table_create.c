2feb52a14360fc1b0f83ea29a0184fc419764491
Evans Manyala <evans.manyala@gmail.com>
2023-12-23 02:06:03 +0300

    Fixing error in code

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,23 +9,17 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *newHashTable = NULL;
-	unsigned long int x = 0;
 
-	newHashTable = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (size == 0)
+		return (NULL);
+	newHashTable = malloc(sizeof(hash_table_t));
 
-	if (newHashTable == NULL)
+	if (!newHashTable)
 		return (NULL);
 	newHashTable->size = size;
-	newHashTable->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+	newHashTable->array = calloc((size_t)size, sizeof(hash_node_t *));
 
 	if (newHashTable->array == NULL)
-	{
-		free(newHashTable);
 		return (NULL);
-	}
-	for (x = 0; x < size; x++)
-	{
-		newHashTable->array[x] = NULL;
-	}
 	return (newHashTable);
 }
