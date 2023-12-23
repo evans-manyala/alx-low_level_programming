2ad8c9486aafa7738dc8895a34e3684d78324e80
Evans Manyala <evans.manyala@gmail.com>
2023-12-23 03:26:12 +0300

    Adding final error fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	newHashTable->size = size;
 	newHashTable->array = calloc((size_t)size, sizeof(hash_node_t *));
 
-	if (newHashTable->array)
+	if (!newHashTable->array)
 	{
 		free(newHashTable);
 		return (NULL);
