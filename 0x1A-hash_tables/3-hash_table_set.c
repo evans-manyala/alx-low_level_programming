b6058c45e6375c0e5f0195ab2a3b24c1d5db369a
Evans Manyala <evans.manyala@gmail.com>
2023-12-23 03:10:22 +0300

    Adding script file

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,50 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - Adds or updates an element in the hash table
+ * @ht: The hash table
+ * @key: The key
+ * @value: The value associated with the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = key_index((unsigned char *)key, ht->size);
+	hash_node_t *new_node = NULL;
+	hash_node_t *current = ht->array[index];
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			if (current->value == NULL)
+				return (0);
+			return (1);
+		}
+		current = current->next;
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return (0);
+	}
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
