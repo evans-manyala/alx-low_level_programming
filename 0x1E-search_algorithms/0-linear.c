c8d670931744e78dccb883d9195efeef43847d6c
evans-manyala <evans.manyala@gmail.com>
2024-04-09 15:59:04 +0300

    Fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,7 +5,7 @@
  * @array: Array being searched
  * @size: Size of the array
  * @value: Unique value in the array to be located
- * Return: Value found or none.
+ * Return: Value found or -1 if not found
 */
 int linear_search(int *array, size_t size, int value)
 {
