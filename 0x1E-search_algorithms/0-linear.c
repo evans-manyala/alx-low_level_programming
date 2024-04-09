92dd2470063226e779ca86d582827851fedcded5
evans-manyala <evans.manyala@gmail.com>
2024-04-09 12:47:33 +0300

    Adding code logic

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+/**
+ * linear_search - function that searches for a value in
+ * an array of integers using the Linear search algorithm
+ * @array: Array being searched
+ * @size: Size of the array
+ * @value: Unique value in the array to be located
+ * Return: Value found or none.
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for(i = 0; i < size; i++)
+	{
+		printf("Comparing value in array[%lu]: %d\n", i, array[i]);
+
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
