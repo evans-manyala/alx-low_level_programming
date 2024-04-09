6b1b81fe3dcb8101cfc1d3a751cc0d797f817026
evans-manyala <evans.manyala@gmail.com>
2024-04-09 14:19:22 +0300

    Error fixes

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,15 +9,17 @@
 */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	size_t i = 0;
 
 	if (array != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
-			printf("Value checked array[%lu]= [%d]\n", i, array[i]);
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 			if (array[i] == value)
-			return (i);
+			{
+				return (i);
+			}
 		}
 	}
 	return (-1);
