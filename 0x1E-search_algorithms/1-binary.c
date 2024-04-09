9e4326683ee4052f81adf81495c989a9d86bdccc
evans-manyala <evans.manyala@gmail.com>
2024-04-09 16:02:28 +0300

    Initial commit

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+/**
+ * binary_search - Function searches for a value in a
+ * sorted array of integers using the Binary search algorithm
+ * @array: Array to be searched.
+ * @size: Size of the array
+ * @value: Unique value in the array to be located
+ * Return: Value found or -1 if not found or NULL
+*/
+
+int binary_search(int *array, size_t size, int value)
+{
+
+}
