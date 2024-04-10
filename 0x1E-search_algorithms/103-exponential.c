ee353309cd27dbba20c59310c1832cd3554cf3e2
evans-manyala <evans.manyala@gmail.com>
2024-04-10 13:22:51 +0300

    Adding error fix to code logic

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,36 +1,39 @@
 #include "search_algos.h"
 /**
- * binary_search - Function searches for a value in a
+ * binarySearch - Function searches for a value in a
  * sorted array of integers using the Binary search algorithm
- * @array: Array to be searched.
- * @size: Size of the array
- * @value: Unique value in the array to be located
+ * @array: Array is a pointer to the first element of the array to search in
+ * @low: Left index in the array
+ * @high: Right index in the array
+ * @value: Value to search for in the array
+ * 
  * Return: Value found or -1 if not found or NULL
 */
 
-int binary_search(int *array, size_t size, int value)
+int binarySearch(int *array, int low, int high, int value)
 {
-	size_t i, left, right;
+	int middle, i;
 
-	if (array == NULL)
+    if (array == NULL || low > high)
 		return (-1);
 
-	for (left = 0, right = size - 1; right >= left;)
+	while (low <= high)
 	{
+		middle = low + (high - low) / 2;
 		printf("Searching in array: ");
-		for (i = left; i < right; i++)
+
+		for (i = low; i < high; i++)
 			printf("%d, ", array[i]);
+
 		printf("%d\n", array[i]);
 
-		i = left + (right - left) / 2;
-		if (array[i] == value)
-			return (i);
-		if (array[i] > value)
-			right = i - 1;
+		if (array[middle] == value)
+			return (middle);
+		else if (array[middle] < value)
+			low = middle + 1;
 		else
-			left = i + 1;
+			high = middle - 1;
 	}
-
 	return (-1);
 }
 
@@ -59,6 +62,6 @@ int exponential_search(int *array, size_t size, int value)
 	}
 	min = (edge < size) ? edge : size;
 	printf("Value found between indexes [%lu] and [%lu]\n", edge / 2, min - 1);
-	result = binary_search(array, min, value);
+	result = binarySearch(array, edge/2, min, value);
 	return (result);
 }
