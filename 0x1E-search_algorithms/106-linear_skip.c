9062253ef7b7f6eb654455686ee8f56217a59971
evans-manyala <evans.manyala@gmail.com>
2024-04-10 20:11:15 +0300

    Initial commit

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+/**
+ * linear_skip - Function searches for a value in a sorted
+ * skip list of integers.
+ * @list: Linked list to tbe skipped
+ * @value: Item being searched for in the linked list
+ * Return:
+ *
+*/
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+
+}
